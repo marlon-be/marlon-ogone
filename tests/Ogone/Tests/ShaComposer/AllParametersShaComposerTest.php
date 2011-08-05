51c0ee7f619e7f5d1030f451d6416c2e8847eaa2
Mathias Verraes <mathias@verraes.net>
2011-08-05 15:12:14 +0200

    added another set of testdata

--- tests/Ogone/Tests/ShaComposer/AllParametersShaComposerTest.php
+++ tests/Ogone/Tests/ShaComposer/AllParametersShaComposerTest.php
@@ -6,25 +6,24 @@ use Ogone\ShaComposer\AllParametersShaComposer;
 
 class AllParametersShaComposerTest extends \TestCase
 {
-	const PASSPHRASE = 'Mysecretsig1875!?';
-	const SHASTRING = 'B209960D5703DD1047F95A0F97655FFE5AC8BD52';
-
-	/** @test */
-	public function ShaStringCanBeComposed()
+	/**
+	 * @test
+	 * @dataProvider provideRequest
+	 */
+	public function ShaStringIsComposedCorrectly(PassPhrase $passphrase, array $request, $expectedSha)
 	{
-		$aRequest = $this->provideRequest();
-
-		$composer = new AllParametersShaComposer(new Passphrase(self::PASSPHRASE));
-		$shaString = $composer->compose($aRequest);
-
-		$this->assertEquals(self::SHASTRING, $shaString);
+		$composer = new AllParametersShaComposer($passphrase);
+		$this->assertEquals($expectedSha, $composer->compose($request));
 	}
 
-	private function provideRequest()
+	public function provideRequest()
 	{
 		// @todo also test unknown params, empty params
 
-		return array(
+		$passphrase = new PassPhrase('Mysecretsig1875!?');
+
+		$expectedSha1 = 'B209960D5703DD1047F95A0F97655FFE5AC8BD52';
+		$request1 = array(
 			'currency' => 'EUR',
 			'ACCEPTANCE' => 1234,
 			'amount' => 15,
@@ -36,5 +35,32 @@ class AllParametersShaComposerTest extends \TestCase
 			'STATUS' => 9,
 			'orderID' => 12
 		);
+
+		$expectedSha2 = 'D58400479DCEDD6B6C7E67D61FDC0CC9E6ED65CB';
+		$request2 = array (
+			'orderID' => 'myorderid1678834094',
+			'currency' => 'EUR',
+			'amount' => '99',
+			'PM' => 'CreditCard',
+			'ACCEPTANCE' => 'test123',
+			'STATUS' => '9',
+			'CARDNO' => 'XXXXXXXXXXXX1111',
+			'ED' => '0312',
+			'CN' => 'Some Name',
+			'TRXDATE' => '01/10/11',
+			'PAYID' => '9126297',
+			'NCERROR' => '0',
+			'BRAND' => 'VISA',
+			'COMPLUS' => 'my feedbackmessage',
+			'IP' => '12.123.12.123',
+		);
+
+		return array(
+			array($passphrase, $request1, $expectedSha1),
+			array($passphrase, $request2, $expectedSha2),
+		);
+
 	}
+
+
 }
