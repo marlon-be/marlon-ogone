d8ddea0fcda05a489bf20282368441586b4e1ed7
Mathias Verraes <mathias@verraes.net>
2011-08-08 11:38:48 +0200

    completed test for SimpleFormGenerator

--- lib/Ogone/FormGenerator/template/simpleForm.php
+++ lib/Ogone/FormGenerator/template/simpleForm.php
@@ -2,10 +2,10 @@
 
 	<?php foreach($this->getParameters() as $key => $value) :?>
 		<?php if($value) :?>
-			<input type="hidden" name="<?php echo $key?>" value="<?php echo htmlspecialchars($value) ?>">
+			<input type="hidden" name="<?php echo $key?>" value="<?php echo htmlspecialchars($value) ?>"  />
 		<?php endif?>
 	<?php endforeach?>
 	<input type="hidden" name="SHASIGN" value="<?php echo $this->getShaSign()?>" />
-	<input type="submit" value="Submit" id="submit" name="submit">
+	<input type="submit" value="Submit" id="submit" name="submit" />
 
 </form>
