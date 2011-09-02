de9134f2986751347a7fd8a8ef3e02e85b2f009d
Jelte Steijaert <jelte@marlon.be>
2011-09-02 10:42:51 +0200

    fix autoload

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 use Symfony\Component\ClassLoader\UniversalClassLoader;
-require_once __DIR__ . '/vendors/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__ . '/vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $loader = new UniversalClassLoader;
 $loader->registerNamespaces(array(
