9c09a457b9c3fe46eb754b52d9536491623f8b52
9Lives Development <info@9lives-development.com>
2012-07-30 16:08:25 +0100

    Added functions to set subscription parameters Allowed amount = 0 in case a subscription is initiated (see PaymentRequest.php and simpleForm.php)

--- lib/Ogone/PaymentRequest.php
+++ lib/Ogone/PaymentRequest.php
@@ -21,6 +21,8 @@ class PaymentRequest
 {
 	const TEST = "https://secure.ogone.com/ncol/test/orderstandard_utf8.asp";
 	const PRODUCTION = "https://secure.ogone.com/ncol/prod/orderstandard_utf8.asp";
+	const SINGLE = 'single';
+	const SUBSCRIPTION = 'subscription';
 
 	private $brandsmap = array(
 		'Acceptgiro' => 'Acceptgiro',
@@ -79,6 +81,8 @@ class PaymentRequest
 
 	private $ogoneUri = self::TEST;
 
+	private $paymentType = self::SINGLE;
+
 	private $parameters = array();
 
 	private $ogoneFields = array(
@@ -92,6 +96,11 @@ class PaymentRequest
 	private $requiredfields = array(
 		'pspid', 'currency', 'amount', 'orderid'
 		// 'pm', 'brand' // left out because when missing, ogone will ask the customer for these
+	);
+	
+	private $requiredSubscriptionFields = array(
+		'SUBSCRIPTION_ID', 'SUB_AMOUNT', 'SUB_COM', 'SUB_ORDERID', 'SUB_PERIOD_UNIT',
+		'SUB_PERIOD_NUMBER', 'SUB_PERIOD_MOMENT','SUB_STARTDATE', 'SUB_ENDDATE', 'SUB_STATUS'
 	);
 
 	/** Note this is public to allow easy modification, if need be. */
@@ -136,6 +145,14 @@ class PaymentRequest
 		$this->validateUri($ogoneUri);
 		$this->ogoneUri = $ogoneUri;
 	}
+	
+	public function setPaymentType($paymentType)
+	{
+		if (!in_array($paymentType, array(self::SINGLE, self::SUBSCRIPTION))) {
+			throw new InvalidArgumentException("Invalid payment type");
+		} 
+		$this->paymentType = $paymentType;
+	}
 
 	public function setPspid($pspid)
 	{
@@ -175,15 +192,25 @@ class PaymentRequest
 	 */
 	public function setAmount($amount)
 	{
+
 		if(!is_int($amount)) {
 			throw new InvalidArgumentException("Integer expected. Amount is always in cents");
 		}
-		if($amount <= 0) {
-			throw new InvalidArgumentException("Amount must be a positive number");
+		// We will allow an initial payment of 0 for subscriptions
+		if (self::SUBSCRIPTION == $this->paymentType) {
+			if($amount < 0) {
+				throw new InvalidArgumentException("Amount must be a positive number or 0");
+			}
+		} else {
+			if($amount <= 0) {
+				throw new InvalidArgumentException("Amount must be a positive number");
+			}
 		}
 		if($amount >= 1.0E+15) {
 			throw new InvalidArgumentException("Amount is too high");
 		}
+		
+		
 		$this->parameters['amount'] = $amount;
 
 	}
@@ -376,13 +403,242 @@ class PaymentRequest
 		$this->parameters['tp'] = $tp;
 	}
 
+
+	/** Subscription parameters */
+	
+	/**
+	 * Unique identifier of the subscription. The subscription id must be assigned dynamically.
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param string $subscriptionId (maxlength 50)
+	 */
+	public function setSubscriptionId($subscriptionId)
+	{
+		if(strlen($subscriptionId) > 50) {
+			throw new InvalidArgumentException("Subscription id cannot be longer than 50 characters");
+		}
+		if(preg_match('/[^a-zA-Z0-9_-]/', $subscriptionId)) {
+			throw new InvalidArgumentException("Subscription id cannot contain special characters");
+		}
+		$this->parameters['SUBSCRIPTION_ID'] = $subscriptionId;
+		$this->paymentType = self::SUBSCRIPTION;
+	}
+	
+	/**
+	 * Amount of the subscription (can be different from the amount of the original transaction) 
+	 * multiplied by 100, since the format of the amount must not contain any decimals or other separators.
+	 *
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param integer amount
+	 */	
+	public function setSubscriptionAmount($amount)
+	{
+		if(!is_int($amount)) {
+			throw new InvalidArgumentException("Integer expected. Amount is always in cents");
+		}
+		if($amount <= 0) {
+			throw new InvalidArgumentException("Amount must be a positive number");
+		}
+		if($amount >= 1.0E+15) {
+			throw new InvalidArgumentException("Amount is too high");
+		}
+		$this->parameters['SUB_AMOUNT'] = $amount;	
+		$this->paymentType = self::SUBSCRIPTION;
+	}
+	
+	/**
+	 * Order description
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param string $description (maxlength 100)
+	 */	
+	public function setSubscriptionDescription($description)
+	{
+		if(strlen($description) > 100) {
+			throw new InvalidArgumentException("Subscription description cannot be longer than 50 characters");
+		}
+		if(preg_match('/[^a-zA-Z0-9_- ]/', $description)) {
+			throw new InvalidArgumentException("Subscription description cannot contain special characters");
+		}
+		$this->parameters['SUB_COM'] = $description;
+		$this->paymentType = self::SUBSCRIPTION;
+	}
+	
+	/**
+	 * OrderID for subscription payments
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param string $orderId (maxlength 40)
+	 */	
+	public function setSubscriptionOrderId($orderId)
+	{
+		if(strlen($orderId) > 40) {
+			throw new InvalidArgumentException("Subscription order id cannot be longer than 40 characters");
+		}
+		if(preg_match('/[^a-zA-Z0-9_-]/', $orderId)) {
+			throw new InvalidArgumentException("Subscription order id cannot contain special characters");
+		}
+		$this->parameters['SUB_ORDERID'] = $orderId;
+		$this->paymentType = self::SUBSCRIPTION;	
+	}
+	
+	/**
+	 * Set subscription payment interval 
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param string $unit 			(‘d’ = daily, ‘ww’ = weekly, ‘m’ = monthly)
+	 * @param integer $interval 	Interval between each occurrence of the subscription payments.
+	 * @param integer $moment		Depending on sub_period_unit
+	 *								Daily (d)
+	 *								interval in days
+	 *								Weekly (ww)
+	 * 								1=Sunday, … 7=Saturday
+	 *								Monthly (m)
+	 * 								day of the month
+	 */
+	public function setSubscriptionPeriod($unit, $interval, $moment)
+	{
+		// Check unit
+		if (!in_array($unit, array('d', 'ww', 'm'))) {
+			throw new InvalidArgumentException("Subscription period unit should be d (daily), ww (weekly) or m (monthly)");
+		}
+		$this->parameters['SUB_PERIOD_UNIT'] = $unit;
+		
+		
+		// Check interval
+		if(!is_int($interval)) {
+			throw new InvalidArgumentException("Integer expected for interval");
+		}
+		if($interval < 0) {
+			throw new InvalidArgumentException("Interval must be a positive number > 0");
+		}
+		if($interval >= 1.0E+15) {
+			throw new InvalidArgumentException("Interval is too high");
+		}
+		$this->parameters['SUB_PERIOD_NUMBER'] = $interval;
+		
+		// Check moment
+		if(!is_int($moment)) {
+			throw new InvalidArgumentException("Integer expected for moment");
+		}
+		if($moment <= 0) {
+			throw new InvalidArgumentException("Moment must be a positive number");
+		}
+		
+		if ('ww' == $unit) {
+			// Valid values are 1 to 7
+			if ($moment > 7) {
+				throw new InvalidArgumentException("Moment should be 1 (Sunday), 2, 3 .. 7 (Saturday)");
+			}
+		} elseif ('m' == $unit) {
+			// We will not allow a day of month > 28
+			if ($moment > 28) {
+				throw new InvalidArgumentException("Moment can't be larger than 29. Last day for month allowed is 28.");
+			}
+		}
+		$this->parameters['SUB_PERIOD_MOMENT'] = $moment;
+		$this->paymentType = self::SUBSCRIPTION;	
+	}
+	
+	
+	/**
+	 * Subscription start date
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param date $data 	Startdate of the subscription. Format yyyy-mm-dd
+	 */
+	public function setSubscriptionStartdate($date)
+	{
+		if (preg_match ("/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/", $date, $parts))
+  		{
+    		//check weather the date is valid of not
+			if(checkdate($parts[2],$parts[3],$parts[1])) {
+	  			$this->parameters['SUB_STARTDATE'] = $date;
+	  			$this->paymentType = self::SUBSCRIPTION;
+			}
+			else {
+				throw new InvalidArgumentException("Invalid date specified for subscription start date.");
+			}
+  		} else {
+  			throw new InvalidArgumentException("Invalid date format for subscription start date. Allowed format: yyyy-mm-dd");
+  		}
+	}
+	
+	/**
+	 * Subscription end date
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param date $data 	Enddate of the subscription. Format yyyy-mm-dd
+	 */
+	public function setSubscriptionEnddate($date)
+	{
+		if (preg_match ("/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/", $date, $parts))
+  		{
+    		//check weather the date is valid of not
+			if(checkdate($parts[2],$parts[3],$parts[1])) {
+	  			$this->parameters['SUB_ENDDATE'] = $date;
+	  			$this->paymentType = self::SUBSCRIPTION;
+			}
+			else {
+				throw new InvalidArgumentException("Invalid date specified for subscription end date.");
+			}
+  		} else {
+  			throw new InvalidArgumentException("Invalid date format for subscription end date. Allowed format: yyyy-mm-dd");
+  		}
+	}	
+	
+	/**
+	 * Set subscription status
+	 * @author René de Kat <renedekat@9lives-development.com>	 
+	 *
+	 * @param integer $status	0 = inactive, 1 = active
+	 */
+	public function setSubscriptionStatus($status)
+	{		
+		if (!in_array($status, array(0, 1))) {
+			throw new InvalidArgumentException("Invalid status specified for subscription. Possible values: 0 = inactive, 1 = active");
+		}
+		$this->parameters['SUB_STATUS'] = $status;
+		$this->paymentType = self::SUBSCRIPTION;
+	}
+	
+	/**
+	 * Set comment for merchant
+	 * @author René de Kat <renedekat@9lives-development.com>
+	 *
+	 * @param string $comment
+	 */
+	public function setSubscriptionComment($comment)
+	{
+		if(strlen($comment) > 200) {
+			throw new InvalidArgumentException("Subscription comment cannot be longer than 200 characters");
+		}
+		if(preg_match('/[^a-zA-Z0-9_- ]/', $comment)) {
+			throw new InvalidArgumentException("Subscription comment cannot contain special characters");
+		}
+		$this->parameters['SUB_COMMENT'] = $comment;
+		$this->paymentType = self::SUBSCRIPTION;	
+	}
+	
+
 	public function validate()
 	{
 		foreach($this->requiredfields as $field)
 		{
-			if(empty($this->parameters[$field])) {
+			if(!isset($this->parameters[$field])) {
 				throw new RuntimeException("$field can not be empty");
 			}
+		}
+		
+		// Check all mandatory fields for subscriptions
+		if (self::SUBSCRIPTION == $this->paymentType) {
+			foreach($this->requiredSubscriptionFields as $field)
+			{			
+				if(!isset($this->parameters[$field])) {
+					throw new RuntimeException("$field can not be empty");
+				}
+			}	
 		}
 	}
 
