92a4e4caa4c0babcc0ee888b3ba94ea1366afb5b
Camilo Sperberg <c.sperberg@bedandbreakfast.eu>
2015-10-07 14:52:51 +0200

    Setting a homeurl is now supported as well
    Very handy for scenarios where you have multiple sites going through
    a common OGONE account

--- lib/Ogone/AbstractRequest.php
+++ lib/Ogone/AbstractRequest.php
@@ -37,7 +37,7 @@ abstract class AbstractRequest implements Request
     protected $ogoneFields = array(
         'pspid', 'orderid', 'com', 'amount', 'currency', 'language', 'cn', 'email',
         'cardno', 'cvc', 'ed', 'ownerzip', 'owneraddress', 'ownercty', 'ownertown',
-        'ownertelno', 'accepturl', 'declineurl', 'exceptionurl', 'cancelurl', 'backurl',
+        'ownertelno', 'homeurl', 'accepturl', 'declineurl', 'exceptionurl', 'cancelurl', 'backurl',
         'complus', 'paramplus', 'pm', 'brand', 'title', 'bgcolor', 'txtcolor', 'tblbgcolor',
         'tbltxtcolor', 'buttonbgcolor', 'buttontxtcolor', 'logo', 'fonttype', 'tp', 'paramvar',
         'alias', 'aliasoperation', 'aliasusage', 'aliaspersistedafteruse'
@@ -90,6 +90,14 @@ abstract class AbstractRequest implements Request
     public function setCn($cn)
     {
         $this->parameters['cn'] = str_replace(array("'", '"'), '', $cn); // replace quotes
+    }
+
+    public function setHomeurl($homeurl)
+    {
+        if (!empty($homeurl)) {
+            $this->validateUri($homeurl);
+        }
+        $this->parameters['homeurl'] = $homeurl;
     }
 
     public function setAccepturl($accepturl)
